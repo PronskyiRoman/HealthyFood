220efc7d9265216e0923bd188840601c0d1b301b
@Roman Pronskyi <roman.pronskyi@itomy.ch>
2022-02-19 17:51:07 +0200

    initial project

---
+++ HealthyFood/Shared/HealthyFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyFoodApp.swift
+//  Shared
+//
+//  Created by Roman Pronskyi on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthyFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
